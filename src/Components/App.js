7a9b699dac2d0dc51df100a6b2a9ba4c5fa3aaf1
hyunduk-jo <johd321@kakao.com>
2020-11-19 22:36:23 +0900

    Frontend Setup #1

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import GlobalStyles from '../Styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import Theme from '../Styles/Theme';
+
+/* // eslint-disable-next-line
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    hello
+  </ThemeProvider>
+) */
+
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={Theme}>
+        <GlobalStyles />
+        Hello
+      </ThemeProvider>
     </div>
-  );
+  )
 }
 
 export default App;
